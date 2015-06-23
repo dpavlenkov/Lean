d772016df95710eee1b0f3b80572c43f6e7cd153
Dmitri Pavlenkov <dmitripavlenkov@gmail.com>
2015-06-23 18:09:00 -0400

    .

--- Common/Managed Orders/ManagedOrder.cs
+++ Common/Managed Orders/ManagedOrder.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuantConnect.Orders;
 
-namespace QuantConnect
+namespace QuantConnect.ManagedOrders
 {
     public class ManagedOrder
     {
+        Guid Id;
+        Guid? AttachedToId;
+
+        Guid[] OneCancelsAllGroupIds;
+
+        Order UnderlyingOrder;
+
     }
 }
